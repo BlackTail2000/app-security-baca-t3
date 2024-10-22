aab3f85bd8b14574870ebad6dbae131003b53c43
BlackTail2000 <carlosgabrielbacamanrique@hotmail.com>
2024-10-21 19:53:29 -0500

    Proyecto Creado

---
+++ src/main/java/pe/edu/cibertec/app_security_baca_t3/AppSecurityBacaT3Application.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.app_security_baca_t3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSecurityBacaT3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSecurityBacaT3Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/app_security_baca_t3/AppSecurityBacaT3ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.app_security_baca_t3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSecurityBacaT3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
